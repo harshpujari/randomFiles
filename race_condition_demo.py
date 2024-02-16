f7c3f91d4ae46369f18fcc3e3c7d6d4d48d99e8b
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2024-02-16 23:12:08 +0530

    Update race_condition_demo.py

--- race_condition_demo.py
+++ race_condition_demo.py
@@ -2,7 +2,7 @@ import threading
 # Shared variable.
 counter = 0
 
-# Function to increment the counter
+# Function to increment the counter.
 def increment_counter():
     global counter
     for _ in range(1000000):
